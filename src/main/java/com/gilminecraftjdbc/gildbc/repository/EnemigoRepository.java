906fe501fe95462a47dddadc9c2fc1220c7b4143
Johnd3r <johndersanchez001@gmail.com>
2025-06-10 15:54:20 -0500

    Refactor EnemigoRepository queries and update HTML templates for enemy and mission management

--- src/main/java/com/gilminecraftjdbc/gildbc/repository/EnemigoRepository.java
+++ src/main/java/com/gilminecraftjdbc/gildbc/repository/EnemigoRepository.java
@@ -10,13 +10,13 @@ import com.gilminecraftjdbc.gildbc.model.Mision;
 
 // En EnemigoRepository
 public interface EnemigoRepository extends CrudRepository<Enemigo, Long> {
-    @Query("SELECT e.* FROM enemigo e JOIN misionEnemigo me ON e.idEnemigo = me.idEnemigo WHERE me.idMision = :idMision")
+    @Query("SELECT e.* FROM enemigo e JOIN misionenemigo me ON e.idEnemigo = me.idEnemigo WHERE me.idMision = :idMision")
     List<Enemigo> findByMisionId(@Param("idMision") Long idMision);
 
-    @Query("SELECT m.* FROM mision m JOIN mision_enemigo me ON m.id_mision = me.id_mision WHERE me.id_enemigo = :idEnemigo")
-    List<Mision> findMisionesByEnemigoId(@Param("idEnemigo") Long idEnemigo);
+    @Query("SELECT m.* FROM mision m JOIN misionenemigo me ON m.idMision = me.idMision WHERE me.idEnemigo = :idEnemigo")
+List<Mision> findMisionesByEnemigoId(@Param("idEnemigo") Long idEnemigo);
 
-    @Modifying
-    @Query("INSERT INTO mision_enemigo (id_mision, id_enemigo, cantidad) VALUES (:idMision, :idEnemigo, :cantidad)")
-    void asignarAMision(Long idMision, Long idEnemigo, int cantidad);
+@Modifying
+@Query("INSERT INTO misionenemigo (idMision, idEnemigo, cantidad) VALUES (:idMision, :idEnemigo, :cantidad)")
+void asignarAMision(Long idMision, Long idEnemigo, int cantidad);
 }
